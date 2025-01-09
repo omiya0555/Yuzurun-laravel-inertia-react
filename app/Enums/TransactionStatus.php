748e92b819c7dc15387b804f52245398c789ed82
omiya0555 <otoharadak@gmail.com>
2025-01-09 11:30:42 +0900

    The Enums class has been created to manage status and messages.

---
+++ app/Enums/TransactionStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enums;
+
+enum TransactionStatus: int
+{
+    case PENDING    = 0;       // 出品中
+    case BOOKING    = 1;       // 予約中
+    case COMPLETED  = 2;       // 取引完了
+
+    /**
+     * ステータスのラベルを取得
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::PENDING   => '出品中',
+            self::BOOKING   => '予約中',
+            self::COMPLETED => '取引完了',
+        };
+    }
+
+    /**
+     * ステータス変更に伴うメッセージ
+     */
+    public function message(string $userName): string
+    {
+        return match ($this) {
+            self::PENDING => '商品を出品しました。',
+            self::BOOKING => '商品が予約されました。',
+            self::COMPLETED => '取引が完了しました。ありがとうございました！',    
+       };
+    }
+}
