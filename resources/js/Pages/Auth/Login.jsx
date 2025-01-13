f6d6ece6c32368d486ca41bc564feff136eb79e8
omiya0555 <otoharadak@gmail.com>
2025-01-13 22:53:56 +0900

    Add the title.

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -33,6 +33,12 @@ export default function Login({ status, canResetPassword }) {
 
             <form onSubmit={submit}>
                 <div>
+                    <h1 className='flex justify-center text-3xl mt-4 mb-8 font-sans font-semibold text-stone-800'>
+                        <span className='text-red-500'>Yuzu</span>
+                        <span className='text-blue-500'>run</span>
+                    </h1>
+                </div>
+                <div>
                     <InputLabel htmlFor="email" value="Eメール" />
 
                     <TextInput
@@ -65,7 +71,7 @@ export default function Login({ status, canResetPassword }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4 block">
+                <div className="mt-4 mx-2 block">
                     <label className="flex items-center">
                         <Checkbox
                             name="remember"
@@ -80,10 +86,10 @@ export default function Login({ status, canResetPassword }) {
                     </label>
                 </div>
 
-                <div className="mt-4 flex items-center justify-end">
+                <div className="mt-4 mx-2 flex items-center justify-end space-x-5">
                     <Link
                         href={route('register')}
-                        className="mr-4 rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                         新規登録
                     </Link>
@@ -96,11 +102,32 @@ export default function Login({ status, canResetPassword }) {
                         </Link>
                     )}
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
+                    <PrimaryButton disabled={processing}>
                         ログイン
                     </PrimaryButton>
                 </div>
             </form>
+
+            {/* GoogleとSlack認証のセクション */}
+            <hr className="my-6 border-t border-gray-300" />
+
+            <div className="flex space-x-4">
+                <button
+                    onClick={() => window.location.href = route('auth.google.redirect')}
+                    className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-red-500 hover:text-white hover:shadow-lg hover:font-bold rounded-md transition duration-300"
+                >
+                    <img src="/images/google.svg" alt="Google Icon" className="w-5 h-5 mr-2" />
+                    Google認証
+                </button>
+
+                <button
+                    onClick={() => window.location.href = route('auth.slack.redirect')}
+                    className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-blue-700 hover:text-white rounded-md hover:shadow-lg hover:font-bold transition duration-300"
+                >
+                    <img src="/images/slack.svg" alt="Slack Icon" className="w-5 h-5 mr-2" />
+                    Slack認証
+                </button>
+            </div>
         </GuestLayout>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -27,6 +27,12 @@ export default function Register() {
 
             <form onSubmit={submit}>
                 <div>
+                    <h1 className='flex justify-center text-3xl mt-4 mb-8 font-sans font-semibold text-stone-800'>
+                        <span className='text-red-500'>Yuzu</span>
+                        <span className='text-blue-500'>run</span>
+                    </h1>
+                </div>
+                <div>
                     <InputLabel htmlFor="name" value="ユーザー名" />
 
                     <TextInput
@@ -115,6 +121,28 @@ export default function Register() {
                     </PrimaryButton>
                 </div>
             </form>
+
+            
+            {/* GoogleとSlack認証のセクション */}
+            <hr className="my-6 border-t border-gray-300" />
+
+            <div className="flex space-x-4">
+                <button
+                    onClick={() => window.location.href = route('auth.google.redirect')}
+                    className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-red-500 hover:text-white hover:shadow-lg hover:font-bold rounded-md transition duration-300"
+                >
+                    <img src="/images/google.svg" alt="Google Icon" className="w-5 h-5 mr-2" />
+                    Google認証
+                </button>
+
+                <button
+                    onClick={() => window.location.href = route('auth.slack.redirect')}
+                    className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-blue-700 hover:text-white rounded-md hover:shadow-lg hover:font-bold transition duration-300"
+                >
+                    <img src="/images/slack.svg" alt="Slack Icon" className="w-5 h-5 mr-2" />
+                    Slack認証
+                </button>
+            </div>
         </GuestLayout>
     );
 }
