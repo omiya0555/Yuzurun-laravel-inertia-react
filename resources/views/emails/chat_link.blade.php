3bf133209c10d233b9e3ff9975e99cc94a0e1a7c
omiya0555 <otoharadak@gmail.com>
2025-01-14 16:44:41 +0900

    Ajust a mail style.

--- resources/views/emails/chat_link.blade.php
+++ resources/views/emails/chat_link.blade.php
@@ -7,8 +7,16 @@
     <title>チャットリンク通知</title>
 </head>
 <body>
+
+    <h1 styles="display:flex; justify-content:center;">Yuzurun</h1>
+    <hr styles="margin-y:24px;" />
+    <h3>{{ $method }}チャットリンクのご案内</h3>
     <p>こんにちは！Yuzurunをご利用いただきありがとうございます。</p>
     <p>以下のリンクから{{ $method }}を使って友達追加して連絡を取りましょう！</p>
     <p><a href="{{ $link }}" target="_blank">{{ $link }}</a></p>
+    <hr styles="margin-y:24px;" />
+    <p>以下のテンプレートをコピーペーストして活用すると取引がスムーズです。
+    <p>【こんにちは。〇〇です。「〇〇」を〇日の〇時に職場で受け取りたいです。】</p>
+
 </body>
 </html>
