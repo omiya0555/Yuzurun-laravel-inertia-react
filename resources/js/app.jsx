86967e930f0d39c2daeeb1c5ebb8c6d9693a6d06
omiya0555 <otoharadak@gmail.com>
2025-01-14 17:43:40 +0900

    Setup the NProgress in app.jsx,app.css.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,33 @@
 import '../css/app.css';
 import './bootstrap';
-
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; // プログレスバーのスタイルをインポート
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { router } from '@inertiajs/react'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+// NProgress の設定
+NProgress.configure({
+    showSpinner: false, // スピナー非表示
+    minimum: 0.1,       // 初期状態の最小値
+    speed: 500,         // バーの進行速度(ms)
+    trickleSpeed: 200,  // 自動トリクル速度
+});
+
+// 画面遷移イベントのフック
+router.on('start', () => {
+    NProgress.start();
+    overlay.classList.add('active');
+});
+
+router.on('finish', (event) => {
+    if (!event.detail.visit.completed) return; // 完了しない場合、バーを止めない
+    NProgress.done(); 
+    overlay.classList.remove('active'); 
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,10 +38,9 @@ createInertiaApp({
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
-
         root.render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: '#4B5563', 
     },
 });
