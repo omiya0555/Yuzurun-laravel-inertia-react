28057b70c26fc15e94faab14526efabc787e4901
omiya0555 <otoharadak@gmail.com>
2025-01-15 12:12:14 +0900

    Create the Terms of service.

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,11 +1,14 @@
-import InputError from '@/Components/InputError';
+import React, { useState } from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import TermsOfServiceModal from './TermsOfServiceModal'; // 利用規約モーダルのインポート
 
 export default function Register() {
+    const [isModalOpen, setIsModalOpen] = useState(false); // モーダル状態
+    const [isAgreed, setIsAgreed] = useState(false); // 利用規約同意状態
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
         email: '',
@@ -15,26 +18,27 @@ export default function Register() {
 
     const submit = (e) => {
         e.preventDefault();
-
-        post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
-        });
+        if (isAgreed) {
+            post(route('register'), {
+                onFinish: () => reset('password', 'password_confirmation'),
+            });
+        } else {
+            alert('利用規約に同意してください。');
+        }
     };
 
     return (
         <GuestLayout>
             <Head title="Register" />
-
             <form onSubmit={submit}>
                 <div>
-                    <h1 className='flex justify-center text-3xl mt-4 mb-8 font-sans font-semibold text-stone-800'>
-                        <span className='text-red-500'>Yuzu</span>
-                        <span className='text-blue-500'>run</span>
+                    <h1 className="flex justify-center text-3xl mt-4 mb-8 font-sans font-semibold text-stone-800">
+                        <span className="text-red-500">Yuzu</span>
+                        <span className="text-blue-500">run</span>
                     </h1>
                 </div>
                 <div>
                     <InputLabel htmlFor="name" value="ユーザー名" />
-
                     <TextInput
                         id="name"
                         name="name"
@@ -45,13 +49,9 @@ export default function Register() {
                         onChange={(e) => setData('name', e.target.value)}
                         required
                     />
-
-                    <InputError message={errors.name} className="mt-2" />
                 </div>
-
                 <div className="mt-4">
                     <InputLabel htmlFor="email" value="Eメール" />
-
                     <TextInput
                         id="email"
                         type="email"
@@ -62,13 +62,9 @@ export default function Register() {
                         onChange={(e) => setData('email', e.target.value)}
                         required
                     />
-
-                    <InputError message={errors.email} className="mt-2" />
                 </div>
-
                 <div className="mt-4">
                     <InputLabel htmlFor="password" value="パスワード" />
-
                     <TextInput
                         id="password"
                         type="password"
@@ -79,16 +75,9 @@ export default function Register() {
                         onChange={(e) => setData('password', e.target.value)}
                         required
                     />
-
-                    <InputError message={errors.password} className="mt-2" />
                 </div>
-
                 <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="パスワード確認"
-                    />
-
+                    <InputLabel htmlFor="password_confirmation" value="パスワード確認" />
                     <TextInput
                         id="password_confirmation"
                         type="password"
@@ -96,16 +85,28 @@ export default function Register() {
                         value={data.password_confirmation}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) =>
-                            setData('password_confirmation', e.target.value)
-                        }
+                        onChange={(e) => setData('password_confirmation', e.target.value)}
                         required
                     />
+                </div>
 
-                    <InputError
-                        message={errors.password_confirmation}
-                        className="mt-2"
-                    />
+                <div className="mt-4">
+                    <label className="flex items-center space-x-2">
+                        <input
+                            type="checkbox"
+                            className="form-checkbox rounded-sm"
+                            checked={isAgreed}
+                            onChange={() => setIsAgreed(!isAgreed)}
+                        />
+                        <span className="text-sm text-gray-600">利用規約に同意します</span>
+                        <button
+                            type="button"
+                            className="underline text-blue-500 hover:text-blue-700 text-sm"
+                            onClick={() => setIsModalOpen(true)}
+                        >
+                            詳細を確認
+                        </button>
+                    </label>
                 </div>
 
                 <div className="mt-4 flex items-center justify-end">
@@ -116,19 +117,23 @@ export default function Register() {
                         既に登録済みですか？
                     </Link>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
+                    <PrimaryButton className="ms-4" disabled={processing || !isAgreed}>
                         新規登録
                     </PrimaryButton>
                 </div>
             </form>
 
-            
             {/* GoogleとSlack認証のセクション */}
             <hr className="my-6 border-t border-gray-300" />
-
             <div className="flex space-x-4">
                 <button
-                    onClick={() => window.location.href = route('auth.google.redirect')}
+                    onClick={() => {
+                        if (isAgreed) {
+                            window.location.href = route('auth.google.redirect');
+                        } else {
+                            alert('利用規約に同意してください。');
+                        }
+                    }}
                     className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-red-500 hover:text-white hover:shadow-lg hover:font-bold rounded-md transition duration-300"
                 >
                     <img src="/images/google.svg" alt="Google Icon" className="w-5 h-5 mr-2" />
@@ -136,13 +141,22 @@ export default function Register() {
                 </button>
 
                 <button
-                    onClick={() => window.location.href = route('auth.slack.redirect')}
+                    onClick={() => {
+                        if (isAgreed) {
+                            window.location.href = route('auth.slack.redirect');
+                        } else {
+                            alert('利用規約に同意してください。');
+                        }
+                    }}
                     className="flex items-center justify-center w-full py-2 bg-stone-400 hover:bg-blue-700 hover:text-white rounded-md hover:shadow-lg hover:font-bold transition duration-300"
                 >
                     <img src="/images/slack.svg" alt="Slack Icon" className="w-5 h-5 mr-2" />
                     Slack認証
                 </button>
             </div>
+
+            {/* 利用規約モーダル */} 
+            <TermsOfServiceModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         </GuestLayout>
     );
 }
